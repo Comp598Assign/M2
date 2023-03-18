1196d9730c82f24e617460427d43904f44a0b84a
Yijie Zhang <z.yijie0813@gmail.com>
2023-03-17 23:59:56 -0400

    update structure

---
+++ resource_manager.py
@@ -0,0 +1,107 @@
+from io import BytesIO
+from flask import Flask, jsonify, request, render_template
+import pycurl
+import sys
+import json
+import requests
+from urllib.parse import urlencode
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+app = Flask(__name__)
+proxy_url = os.environ.get("ip")
+cURL = pycurl.Curl()
+
+jobs =[]
+jobidcounter_fornextjob =0
+class Job:
+    def __init__(self,status,file,pNode = None):
+        global jobidcounter_fornextjob 
+        self.id = jobidcounter_fornextjob
+        self.status = status
+        self.file = file
+        self.node_running_on = pNode
+        jobidcounter_fornextjob+=1
+    def __str__(self):
+        if self.node_running_on == None:
+            return "Registerd Job waiting in Queque"
+        else:
+            return "Job id:"+self.id +"running in Node: " + self.node_running_on
+    
+def get_av_node():
+    response = requests.get(proxy_url+'/cloudproxy/get_nextav_node')
+    data = response.json()
+    return data["av_node"]
+
+@app.route('/dashboard')
+def dashboard():
+    response = requests.get(proxy_url + '/cloudproxy/allPods/nodes/all')
+    data = response.json()
+    print(data)
+    return render_template("Dashboard.html", data=data['result'])
+
+
+@app.route('/hello', methods = ['GET', 'POST'])
+def cloud():
+    if request.method == "GET":
+        print("A Client says hello")
+        response = "Cloud says hello"
+        return jsonify({"response" : response})
+
+@app.route('/cloud/clusters/initalization') #initalize default cluster
+def cloud_init():
+    response = requests.get(proxy_url + '/cloudproxy/default_cluster/initalization')
+    data = response.json()
+    response = data["response"]
+    return jsonify({"response" : response})
+
+@app.route('/cloud/clusters/default_cluster/<pod_name>', methods = ['POST', 'DELETE', 'GET']) #rigster new pod
+def cloud_pod_registration_remove(pod_name):
+    if request.method == "GET":
+        response = requests.get(proxy_url + '/cloudproxy/default_cluster/' + pod_name)
+        data = response.json()
+        return jsonify(data)
+
+#@app.route('/cloud/nodes/<name>', defaults={'pod_name': 'default'}, methods = ['POST', 'DELETE'])
+@app.route('/cloud/<pod_id>/nodes', methods = ['POST', 'DELETE', 'GET'])
+def cloud_register_delete_node(pod_id):
+    if request.method == "POST":
+        response = requests.post(proxy_url + '/cloudproxy/' + pod_id + '/nodes/' + request.get_json()['node_name'])
+        data = response.json()
+        response = data['node_status'] + ' node named ' + request.get_json()['node_name'] + ' ' + data['result'] + ' under pod ' + str(data['pod_id'])
+        return jsonify({"response" : response})
+
+    elif request.method == "DELETE":
+        response = requests.delete(proxy_url + '/cloudproxy/nodes/' + request.get_json()['node_name'])
+        data = response.json()
+        return jsonify({"response" : data['response']})
+        
+    elif request.method == "GET":
+        response = requests.get(proxy_url + '/cloudproxy/' + pod_id + '/nodes/all')
+        data = response.json()
+        return jsonify({"response" : data['result']})
+
+
+@app.route('/cloud/jobs',methods=['POST'])
+def cloud_launch():
+    if request.method == 'POST':
+        print("request to post file")
+        jobf = request.files['file']
+        #
+        next_node = get_av_node()
+        if next_node == None:
+            job = Job("Registered",jobf)
+            jobs.append(job)
+        else:
+            job = Job("Running",jobf,next_node)
+            response = requests.post(proxy_url+'/cloudproxy/jobs/'+str(job.id)+'/'+next_node,files ={'file':jobf})
+            data = response.json()
+            response =data["response"]
+            print(response)
+            return jsonify({"response" : response})
+        
+
+if __name__ == '__main__':
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.run(debug=True, host='0.0.0.0', port=5000)
---
+++ toolSet.py
@@ -0,0 +1,124 @@
+import pycurl
+import sys
+import requests
+from flask import Flask, jsonify, request
+import os.path
+from urllib.parse import urlencode
+cURL = pycurl.Curl()
+
+def cloud_hello(url):
+    requests.get(url)
+
+    # cURL = pycurl.Curl()
+    # cURL.setopt(cURL.URL, url)
+    # cURL.perform()
+    # cURL.close()
+
+def cloud_init(url): #initalize default cluster
+    requests.get(url + '/cloud/clusters/initalization')
+
+    # cURL = pycurl.Curl()
+    # cURL.setopt(cURL.URL, url + '/cloud/clusters/initalization')
+    # cURL.perform()
+    # cURL.close()
+
+def cloud_all_clasters(url): #show all clusters
+    cURL.setopt(cURL.URL, url + '/cloud/clusters')
+    cURL.perform()
+
+
+def cloud_register_node(url, node_name, pod_id=-1): #TODO specify pod type in request
+    requests.delete(url + '/cloud/' + str(pod_id) + '/nodes', json = {'node_name' : node_name, 'pod_id' : pod_id})
+
+    # cURL = pycurl.Curl()
+    # cURL.setopt(cURL.URL, url + '/cloud/' + str(pod_id) + '/nodes')
+    # cURL.setopt(cURL.POST, 1)
+    # data = {'node_name' : node_name, 'pod_id' : pod_id}
+    # data = urlencode(data)
+    # cURL.setopt(cURL.POSTFIELDS, data)
+    # cURL.perform()
+    # cURL.close()
+
+def cloud_remove_node(url,node_name): #dispatch request to appropriate pod
+    requests.delete(url + '/cloud/-1/nodes', json = {'node_name' : node_name})
+
+
+    # cURL = pycurl.Curl()
+    # cURL.setopt(cURL.URL, url + '/cloud/-1/nodes')
+    # cURL.setopt(cURL.CUSTOMREQUEST, "DELETE")
+    # data = {'node_name' : node_name}
+    # data = urlencode(data)
+    # cURL.setopt(cURL.POSTFIELDS, data)
+    # cURL.perform()
+    # cURL.close()
+
+def cloud_launch_job_with_path(url, file_path):
+    if os.path.isfile(file_path):
+        files = {'file':open(file_path,'rb')}
+        ret = requests.post(url+'/cloud/jobs',files =files)
+        if ret.ok:
+            print("ok")
+        else :
+            print("error")
+
+def cloud_abort_job(url, job_id):
+    #todo
+    return 0
+
+def cloud_launch_pod(url, pod_id):
+    #launch pod
+    return 0
+
+def cloud_resume_pod(url, pod_id):
+    #resume pod
+    #If there are any nodes with the “ONLINE” status, then the Load Balancer should include these in its configuration so that it can start sending traffic through to this node again.
+    return 0
+
+def cloud_pause_pod(url, pod_id):
+    #All the nodes with the “ONLINE” status inside this pod are removed and the Load Balancer is notified so that no more incoming client request on this pod will receive a response
+    return 0
+
+def  cloud_pod_ls(url):
+    response = requests.get(url + '/cloud/clusters/default_cluster/default_pod')
+    data = response.json()
+    for k in data:
+        print("pod: %s, id: %s has %d nodes" % (data[k][0], k, data[k][1]))
+    
+def cloud_node_ls(url, pod_id='allPods'):
+    response = requests.get(url + '/cloud/' + pod_id + '/nodes')
+    data = response.json()
+    for k, v in data["response"].items():
+        print("node: %s; id: %s; status: %s" % (k, v[0], v[1]))
+
+def main():
+    rm_url = sys.argv[1]
+    cloud_init(rm_url)
+    while (1):
+        command = input('$ ')
+        commandstr = command.split()
+        if command == 'cloud init': #TODO: init three pod
+            cloud_init(rm_url)
+        elif command == 'all clusters':
+            cloud_all_clasters(rm_url)
+        elif command.startswith('cloud register') and len(commandstr)==4:
+            cloud_register_node(rm_url,commandstr[2],commandstr[3])
+        elif command.startswith('cloud rm') and len(commandstr)==4:
+            cloud_remove_node(rm_url,commandstr[2],commandstr[3])
+        elif command.startswith('cloud launch') and len(commandstr)==3:
+            #launch pod
+            cloud_launch_job_with_path(rm_url,commandstr[2])
+        elif command.startswith('cloud resume') and len(commandstr)==3:
+            cloud_resume_pod(rm_url,commandstr[2])
+        elif command.startswith('cloud pause') and len(commandstr)==3:
+            cloud_pause_pod(rm_url,commandstr[2])
+        elif command.startswith('cloud abort') and len(commandstr)==3:
+            cloud_abort_job(rm_url,commandstr[2])
+        elif command == 'cloud pod ls':
+            cloud_pod_ls(rm_url)
+        elif command.startswith('cloud node ls') and len(commandstr)==3:
+            cloud_node_ls(rm_url)
+        elif command.startswith('cloud node ls') and len(commandstr)==4:
+            cloud_node_ls(rm_url, commandstr[3])
+
+if __name__ == '__main__':
+    main()
